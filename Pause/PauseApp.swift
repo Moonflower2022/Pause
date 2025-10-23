0292a9226587238fcd36a21637450e6aff344f16
Moonflower2022 <harrisonq125@gmail.com>
2025-10-22 22:00:49 -0700

    Initial Commit

---
+++ Pause/PauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PauseApp.swift
+//  Pause
+//
+//  Created by Harrison Qian on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
